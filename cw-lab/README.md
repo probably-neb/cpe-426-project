a1874fee174496299d3a6ce35ca2ac6dd8f1bd17
Ben Kunkle <ben.kunkle@gmail.com>
2023-11-07 16:41:29 -0800

    include EVERYTHING

---
+++ cw-lab/README.md
@@ -0,0 +1,4 @@
+## LNKS
+- [chipwhiperer git repo](https://github.com/newaetech/chipwhisperer-jupyter)
+- [Chipwhisperer docs](https://chipwhisperer.readthedocs.io/en/latest/virtual-box-inst.html)
+- [Docker connect to device](https://stackoverflow.com/questions/24225647/docker-a-way-to-give-access-to-a-host-usb-or-serial-device)
---
+++ puf-lab/puf-lab.srcs/sources_1/new/RO.vhd
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/21/2023 02:18:24 PM
+-- Design Name: 
+-- Module Name: RO - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity RO is
+  Port (
+
+       );
+end RO;
+
+architecture Behavioral of RO is
+
+begin
+
+
+end Behavioral;
+
+entity ROSlice is
+    Port (
+
+         );
+end ROSlice;
+
+achitecture Behavioral of ROSlice is
+begin
+end Behavioral;
